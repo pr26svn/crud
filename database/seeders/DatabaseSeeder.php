c72a82745d038009deb6d2417cc8b09fa5f569de
pr26svn <pr26sautin@mail.ru>
2023-02-08 00:10:54 +0600

    migrate, seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\newsCompany;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        newsCompany::factory(100)->create();
+        //   newsCompany::factory(100)->create();
+        Post::factory(100)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
